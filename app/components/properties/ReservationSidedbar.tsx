1fc063a7f3443a57dc5a60c35dce54ba5bc1cf98
Shadowsweep <utkarshhg911@gmail.com>
2024-09-01 18:47:21 +0530

    Lec 1 completed

---
+++ app/components/properties/ReservationSidedbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ReservationSidedbar = () => {
+  return (
+    <aside className=' mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl' >
+           <h2 className='mb-5 text-2xl' > Rs. 4000 per night </h2>
+           <div className='mb-5 p-4 border border-gray-400 rounded-xl'>
+            <label className='mb-1 block font-bold text-xs'>Guests</label>
+             <select className='w-full -ml-1 text-xm' >
+             <option>1</option>
+             <option>2</option>
+             <option>3</option>
+             <option>4</option>
+             <option>5</option>
+             </select>
+           </div>
+           <div className='w-full mb-6 py-6 text-center text-white bg-destolive rounded-xl hover:bg-destolive-dark' > Book</div>
+           <div className='mb-4 flex justify-between align-center' >
+            <p>Rs.4000 * 3 nights </p>
+            <p>Rs. 12000</p>
+           </div>
+
+           <div className='mb-4 flex justify-between align-center' >
+            <p>Destolive Fee </p>
+            <p>Rs. 120</p>
+           </div>
+
+           <hr />
+
+           <div className='mt-4 flex justify-between align-center font-bold' >
+            <p>Total</p>
+            <p>Rs. 12120</p>
+           </div>
+
+        </aside>
+  )
+}
+
+export default ReservationSidedbar
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import ReservationSidedbar from '@/app/components/properties/ReservationSidedbar'
 
 const PropertyDetailPage = () => {
   return (
-    <main className=" mt-8 max-w-[1500px] mx-auto px-6 ">
-    <div className='w-full h-[64vh] overflow-hidden rounded-xl relative ' > 
+    <main className=" mt-8 max-w-[1500px] mx-auto px-6 pb-6 ">
+    <div className='w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative ' > 
     <Image 
             fill
             src='/beach.jpeg'
@@ -13,7 +14,7 @@ const PropertyDetailPage = () => {
          />
 
     </div>
-         <div className='mt-4 grid grid-cols-1 md:grid-cols-5 gap-4' >
+         <div className=' grid grid-cols-1 md:grid-cols-5 gap-4' >
               <div className='py-6 pr-6 col-span-3 ' >
                  <h1 className='mb-4 text-4xl ' > Property Name </h1>
                  <span className='mb-6 block text-lg  text-gray-600'>
@@ -23,20 +24,24 @@ const PropertyDetailPage = () => {
                  <div className='py-6 flex items-center space-x-4 ' >
                   <Image 
                     src='/user_profile.png'
-                    width={50}
-                    height={50}
+                    width={75}
+                    height={75}
                     className='rounded-full'
                     alt = "User Image"
                   
                   />
 
                   <p><strong>Tech Unsatisfied</strong> is your host </p>
-                 </div>
+                  </div>
+                  <hr />
+                   <p className='mt-6  text-lg' >
+                        ANy description you wannnt issss sfasfdasfas  sdfdsds f dsfg sdg dgds gds gsdgsdgdgsd  gsdfds  gdfdsfg sdfg  ds sd dgs
+                  </p> 
+                 
+                 
               </div>
-              <div   >
-                 right side
-              </div>
-
+              <ReservationSidedbar/>
+          
          </div>
     
     </main>
