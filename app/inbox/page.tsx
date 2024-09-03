b21c77c8bd253423e12e7722b07b70c493fbeb5e
Shadowsweep <utkarshhg911@gmail.com>
2024-09-03 23:48:06 +0530

    conversation page created

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image"
 import PropertyList from "../components/properties/PropertyList";
+import Conversation from "../components/inbox/Conversation";
 
 const InboxPage  = () => {
     return (
       <main className="mt-6 max-w-[1500px] mx-auto px-6 pb-6">
       <h1 className="mt-4 my-6 mb-2 text-xl">Inbox</h1>
+      <Conversation />
+      <Conversation />
+      <Conversation />
       </main>
     )}
 export default InboxPage ;
